84332a340ba342323ef52a3cf7443be28ec8b1f6
serxxxxx <43133214+serxxxxx@users.noreply.github.com>
2020-10-19 18:20:40 +0300

    Second

--- src/Main.java
+++ src/Main.java
@@ -28,9 +28,22 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        int p = 0;
+        int ne = 0;
+        int nuzhen = 0;
        for(int i = 0; i < 101; i++) {
-            String result = func(new String[]{"Пейсинг", "не", "нужен"}, new int[]{10, 61, 30});
-            System.out.println(result);
+            String result = func(new String[]{"Пейсинг", "не", "нужен"}, new int[]{10,10,10});
+            if (result=="Пейсинг") {
+                 p=p+1;
+            }
+            else if (result=="не") {
+                ne=ne+1;
+            }
+            else if (result=="нужен") {
+                nuzhen=nuzhen+1;
+            }
         }
+        System.out.println("\"Пейсинг\": " + p + ", \"не\": "+ ne+ ", \"нужен\":"+nuzhen);
+
     }
 }
