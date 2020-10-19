d7049f4a0461835ead7b6e1c223da2fcb77aa638
serxxxxx <43133214+serxxxxx@users.noreply.github.com>
2020-10-19 16:51:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+
+
+/*Нужна функция, которая будет возвращать строковое значение
+ из массива в зависимости от массива долей (вероятностей).
+        String func(String[ ] value, int [ ] chance)
+        value - массив значений, chance - массив долей.
+        Пример входных данных:
+        func({"Пейсинг", "не", "нужен"}, {10,61,30});
+        Прогнав функцию N раз ожидаю в статистике увидеть
+        "Пейсинг" 10/101*N раз, "не" 61/101*N раз и т.д.
+*/
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Main {
+    public static String func(String[ ] value, int [ ] chance){
+        String a = "errlogic"; // переменная для отладки
+              for(int i = 0; i < value.length; i++)
+        {
+            int boundedRandomValue = ThreadLocalRandom.current().nextInt(0, 100);
+
+            if ( boundedRandomValue <=chance[i]){
+                a = value[i];
+                break; }
+            chance[i+1]+=chance[i];       }
+
+        return a;
+    }
+
+    public static void main(String[] args) {
+       for(int i = 0; i < 100; i++) {
+            String result = func(new String[]{"Пейсинг", "не", "нужен"}, new int[]{10, 61, 30});
+            System.out.println(result);
+        }
+    }
+}
